a08cc2d07c1ff9fe31a68ef19c68eae8ed58da64
KhanRabiul <krabiulislam2@gmail.com>
2025-01-26 19:53:13 +0600

    search functionality changed to debouce

--- src/components/navigation/Search.jsx
+++ src/components/navigation/Search.jsx
@@ -1,24 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setSearchTerm } from "../../features/filter/filterSlice";
 
-export default function Search(){
-    const [input, setInput] = useState('');
-    const dispatch = useDispatch();
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(setSearchTerm(input))
-    }
-    return (
-        <form onSubmit={handleSubmit} >
-            <input
-                className="outline-none border-none mr-2"
-                type="search"
-                name="search"
-                placeholder="Search"
-                value={input}
-                onChange={(e)=>setInput(e.target.value)}
-            />
-        </form>
-    )
+export default function Search() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      dispatch(setSearchTerm(input));
+    }, 500); 
+
+    return () => clearTimeout(debounce); 
+  }, [input, dispatch]);
+
+  return (
+    <form>
+      <input
+        className="outline-none border-none mr-2"
+        type="search"
+        name="search"
+        placeholder="Search"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </form>
+  );
 }
